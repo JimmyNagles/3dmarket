2842f5f07669620e25c460732a3a376ace8b9130
jimmy nagles <55357484+JimmyNagles@users.noreply.github.com>
2022-11-01 12:19:02 -0400

    updated code

--- components/ModelViewer.jsx
+++ components/ModelViewer.jsx
@@ -1,14 +1,21 @@
 import "@google/model-viewer";
 
-const ModelViewer = ({ glb = "/models/3.glb" }) => (
+const ModelViewer = ({
+  glb = "/models/3.glb",
+  exposure = ".8",
+  shadowIntensity = "1",
+  autoRotate = "false",
+}) => (
   <div className="w-full h-full">
     <model-viewer
+      exposure={exposure}
+      disable-zoom
+      shadow-intensity={shadowIntensity}
       src={glb}
       ios-src=""
       alt="Rocks "
-      shadow-intensity="1"
       camera-controls
-      auto-rotate
+      auto-rotate={autoRotate}
       rotation-per-second="30deg"
       ar-modes="scene-viewer webxr"
       ar
